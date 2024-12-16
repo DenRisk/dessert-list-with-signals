8515d1e62c3293d0d3fb318af1427458e6db1ea9
DenRisk <denisniklas.risken@sybit.de>
2024-12-16 13:55:50 +0100

    fix data add github pages cli

--- src/app/features/desserts/services/desserts.service.ts
+++ src/app/features/desserts/services/desserts.service.ts
@@ -5,7 +5,8 @@ export class DessertsService {
   getDesserts() {
     return resource({
       loader: async () => {
-        const res = await fetch('http://localhost:3000/desserts');
+        const res = await fetch('/data.json');
+        console.log(res)
         return await res.json();
       },
     });
